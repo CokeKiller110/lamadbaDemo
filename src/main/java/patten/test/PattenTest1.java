14451b6202f3117bf50f00a9c0b32ac85657fda0
chenyongsi <mumble-thrill0q@icloud.com>
2023-02-22 14:33:22 +0800

    测试代码提交

---
+++ src/main/java/patten/test/PattenTest1.java
@@ -0,0 +1,36 @@
+package patten.test;
+
+import patten.interfacePackage.PattenTestInterface;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 正则表达式测试
+ */
+public class PattenTest1 {
+    public static void main(String[] args) {
+        PattenTestInterface pattenTestInterface = () -> {
+            Scanner scanner = new Scanner(System.in);
+            String next = "";
+            System.out.println("输入正则表达式：");
+            String next1 = scanner.next();
+            Pattern p = Pattern.compile(next1);
+            System.out.println("测试是否满足[" + next1 + "]此规则");
+            do {
+                System.out.println("请输入要匹配正则表达式的文本(输入'end'结束测试)：");
+                if (scanner.hasNext()) {
+                    next = scanner.next();
+                }
+                if ("end".equals(next)) {
+                    break;
+                }
+                Matcher m = p.matcher(next);
+                boolean b = m.matches();
+                System.out.println("运算结果：" + b);
+            } while (true);
+        };
+        pattenTestInterface.test();
+    }
+}
